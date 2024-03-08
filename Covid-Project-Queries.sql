57b1b0cc975130f874b76f3ee4e19bb7af29d405
lexbucket <157657922+lexbucket@users.noreply.github.com>
2024-03-08 15:45:03 +1100

    Add files via upload

--- Covid-Project-Queries.sql
+++ Covid-Project-Queries.sql
@@ -1,10 +1,15 @@
+/* Dataset source: https://ourworldindata.org/covid-deaths
+The imported file was split in 2 files (covid-deaths and covid-vaccionations) for this project so JOIN statements can be used later on. 
+The objective of this file is showing the use of different statements such as CTEs, temporary tables, Windows funtions, 
+Aggregate functions, Joins, etc. */
+
 -- COVID DEATHS TABLE
--- Data preview
+-- 1. Data preview of the columns we are more interested in
 SELECT Location, date, total_Cases, new_cases, total_DEaths, population
 FROM `covid-deaths`
 ORDER BY 1,2;
 
--- Total Cases vs Total Deaths per country
+-- 2. Total Cases vs Total Deaths in Australia (modify location for other countries)
 -- Likelihood of dying if getting infected (percentage)
 SELECT Location, date, total_cases, new_cases, total_deaths, (total_deaths/total_cases)*100 AS Death_Percentage
 FROM `covid-deaths`
@@ -12,13 +17,15 @@ WHERE LOCATION = 'australia'
 ORDER BY 1,2 ;
 
 
--- Total Cases vs Population per country (keep in mind a person can get infected again so it could count more than once)
-SELECT Location, date, population, total_cases, new_cases, (total_cases/population)*100 AS Infected_Percentage
+-- 3. Total Cases vs Population per country (note a person can get infected more than once)
+SELECT Location, date, 
+	population, total_cases, 
+	new_cases, (total_cases/population)*100 AS Infected_Percentage
 FROM `covid-deaths`
 WHERE LOCATION = 'australia'
 ORDER BY 1,2 ;
 
--- Top 10 Higuest death count vs population per country
+-- 4. Top 10 Higuest death count vs population per country (not a percentage, just quantity)
 SELECT Location, population, MAX(total_deaths) AS Higuest_deaths
 FROM `covid-deaths`
 WHERE continent != ""
@@ -26,8 +33,10 @@ GROUP BY Location, population
 ORDER BY Higuest_deaths DESC 
 LIMIT 10;
 
--- Top 10 Higuest Death rate percentage count vs population per country
-SELECT Location, population, MAX(total_deaths) AS Higuest_deaths, ROUND(MAX((total_deaths/population)*100),4) AS Death_Percentage
+-- 5. Top 10 Higuest Death rate PERCENTAGE count vs population per country
+SELECT Location, population, 
+	MAX(total_deaths) AS Higuest_deaths, 
+    ROUND(MAX((total_deaths/population)*100),4) AS Death_Percentage
 FROM `covid-deaths`
 WHERE continent != ""
 GROUP BY Location, population
@@ -35,7 +44,7 @@ ORDER BY Death_Percentage DESC
 LIMIT 10;
 
 -- NOW THE SAME FOR CONTINENTS
--- Top 10 Higuest death count per continent
+-- 6. Top 10 Higuest death COUNT per continent
 SELECT location, MAX(total_deaths) AS Higuest_deaths
 FROM `covid-deaths`
 WHERE continent = "" 
@@ -44,8 +53,10 @@ GROUP BY Location, population
 ORDER BY Higuest_deaths DESC 
 LIMIT 10;
 
--- Top 10 Higuest Death rate percentage count vs population per continent
-SELECT Location, population, MAX(total_deaths) AS Higuest_deaths, ROUND(MAX((total_deaths/population)*100), 4) AS Death_Percentage
+-- 7. Top 10 Higuest Death rate PERCENTAGE count vs population per continent
+SELECT Location, population, 
+	MAX(total_deaths) AS Higuest_deaths, 
+    ROUND(MAX((total_deaths/population)*100), 4) AS Death_Percentage
 FROM `covid-deaths`
 WHERE continent = ""
 	AND location in ('Europe', 'South America', 'North America', 'Oceania', 'Asia', 'Africa')
@@ -54,14 +65,16 @@ ORDER BY Death_Percentage DESC
 LIMIT 10 ;
 
 -- AROUND THE WORLD
--- PER DATE: Cases, new cases, deaths per day (keep in mind a person can get infected again so 'total cases' are counting people who got infected more than once)
-SELECT date, SUM(total_cases), SUM(new_cases) AS Total_new_cases, SUM(new_deaths) AS total_Deaths, ROUND(SUM(new_Deaths)/SUM(total_CAses)*100, 4) as death_percentage
+-- 8. PER DATE: Cases, new cases, deaths per day (keep in mind a person can get infected again so 'total cases' are counting people who got infected more than once)
+SELECT date, SUM(new_cases) AS Total_new_cases, 
+SUM(total_cases), SUM(new_deaths) AS total_Deaths, 
+ROUND(SUM(new_Deaths)/SUM(total_CAses)*100, 4) as death_percentage
 FROM `covid-deaths`
 WHERE continent != "" 
 GROUP BY date
 ORDER BY date;
 
--- TOTALS: Total cases vs death percentage up until specified "date"
+-- 9. TOTALS: Total cases vs death percentage up until specified "date"
 SELECT SUM(new_cases) AS Total_cases, SUM(new_deaths) AS total_Deaths, ROUND(SUM(new_Deaths)/SUM(new_CAses)*100, 4) as death_percentage
 FROM `covid-deaths`
 WHERE continent != "" AND date < "2024-01-01" -- choose reference date
@@ -70,8 +83,8 @@ WHERE continent != "" AND date < "2024-01-01" -- choose reference date
 ;
 
 
--- COMBINING DEATHS TABLE WITH VACCINATIONS TABLE
--- Adding a Rolling Total using 'New vaccines' to identify total of vaccines (this assume the 'total_vaccinations' column doesn't exist)
+-- COMBINING COVID-DEATHS TABLE WITH COVID-VACCINATIONS TABLE
+-- 10. Adding a Rolling Total using 'New vaccines' to identify total of vaccines (this assume the 'total_vaccinations' column doesn't exist)
 SELECT d.location, d.date, d.population, v.new_vaccinations,
 	SUM(v.new_vaccinations) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rol_tot_vac
 FROM `covid-deaths` d
@@ -81,11 +94,12 @@ JOIN `covid-vaccinations` v
 WHERE d.continent != "" AND d.date < '2024-01-01' AND d.location = "Australia"
 order by 1,2;
 
-/* I want to use the result of the rol_total_vaccin to calculate relation(percentage) with population.
+
+/* 11. I want to use the result of the rol_total_vaccin to calculate relation(percentage) with population.
 However, rol_tot_vac is created in the SELECT and cannot be used again in the same select. There are 2 options to be able to use it.
 IMPORTANT: Total vaccines includes the 2 doses per person and the boosters. It means in one point in history the total 
 vaccinations could be more than double the population*/
--- FIRST: Using CTE
+-- A. Using CTE
 
 With CTE (location, date, population, new_vaccinations, rol_tot_vac)
 	AS
@@ -102,7 +116,7 @@ FROM CTE
 ;
 
 
--- SECOND: Using TEMPORARY TABLES
+-- B. Using TEMPORARY TABLES
 DROP TABLE IF EXISTS TempTable;
 CREATE TABLE TempTable
 (
@@ -126,3 +140,18 @@ order by 1,2;
 SELECT *, (rol_total_vac/population)*100 
 FROM TempTable
 ;
+
+-- 12. Create a VIEW
+CREATE VIEW PercentagePopVaccin AS
+SELECT d.location, d.date, d.population, v.new_vaccinations,
+	SUM(v.new_vaccinations) OVER (PARTITION BY d.location ORDER BY d.location, d.date) AS rol_tot_vac
+FROM `covid-deaths` d
+JOIN `covid-vaccinations` v
+	ON d.location = v.location
+    AND d.date = v.date
+WHERE d.continent != "" AND d.date < '2024-01-01'
+order by 1,2;
+
+-- 13. Test View
+SELECT *
+FROM percentagepopvaccin
